b9acb230c434c4da5095a7b0f6095a357f665ca5
Dhruvit Jalodhara <dhruvitjalodhara@Dhruvits-MacBook-Air.local>
2026-01-01 13:48:05 +0530

    Updated on 01/01/2026

--- src/L09_BinarSearch/C01_BasicBS.java
+++ src/L09_BinarSearch/C01_BasicBS.java
@@ -1,5 +1,22 @@
 package L09_BinarSearch;
 
 public class C01_BasicBS {
+    public static int search(int[] arr, int target) {
+        int n = arr.length;
+        int start = 0 , end = n -1;
+        while(start <= end){
+            int mid = start + (end - start)/2 ;
 
+            if(arr[mid] < target)
+                start = mid + 1;
+            else if(arr[mid] > target)
+                end = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+    }
 }
