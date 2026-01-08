e821cbb98604f563f67da243d897c180d76d4b2e
Dhruvit Jalodhara <dhruvitjalodhara@Dhruvits-MacBook-Air.local>
2026-01-08 14:45:26 +0530

    Updated on 08/01/2026

---
+++ src/L09_BinarSearch/C06_LeetCode2529.java
@@ -0,0 +1,36 @@
+package L09_BinarSearch;
+
+public class C06_LeetCode2529 {
+    public static int maximumCount(int[] arr) {
+        int pos = count(arr , true);  // count positive
+        int neg = count(arr , false); // count negative
+        return Math.max(pos, neg);
+    }
+    public static int count(int[] arr , boolean positive){
+        int start = 0 , end = arr.length - 1;
+        if(positive){
+            while(start <= end){
+                int mid = start + (end - start) / 2;
+                if(arr[mid] <= 0)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return arr.length - start;
+        }else {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] < 0)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return end + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2,-1,-1,1,2,3};
+        System.out.println(maximumCount(arr));
+    }
+}
---
+++ src/L09_BinarSearch/C07_FloorSqrt.java
@@ -0,0 +1,24 @@
+package L09_BinarSearch;
+
+public class C07_FloorSqrt {
+    public static int floorSqrt(int n) {
+        int ans = 0;
+        int start = 1 , end = n;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            long sq = (long)mid * (long)mid;
+            if(sq > n){
+                end = mid - 1;
+            }else if(sq < n){
+                ans = mid;
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(floorSqrt(8));
+    }
+}
