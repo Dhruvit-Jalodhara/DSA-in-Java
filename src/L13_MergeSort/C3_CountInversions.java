b7aa0d7e998258980c11135dc760477310ab405f
Dhruvit Jalodhara <dhruvitjalodhara@Dhruvits-MacBook-Air.local>
2026-01-22 21:51:20 +0530

    Updated on 22/01/2026

--- src/L13_MergeSort/C3_CountInversions.java
+++ src/L13_MergeSort/C3_CountInversions.java
@@ -1,11 +1,16 @@
 package L13_MergeSort;
 
-import java.util.*;
+// Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
 
-// merge sort is perform by divide and conquer
-public class C2_MergeSort1 {
+import java.util.Arrays;
 
-    // TC = O(n*log(n))  ,  SC = O(n*log(n))
+public class C3_CountInversions {
+    static int count;
+    public static int inversionCount(int arr[]) {
+        count = 0;
+        mergeSort(arr);
+        return count;
+    }
     public static void mergeSort(int[] arr){
         int n = arr.length;
 
@@ -29,15 +34,18 @@ public class C2_MergeSort1 {
         int i = 0 , j = 0 , k = 0 ;
         while(i < l1 && j < l2){
             if(a[i] <= b[j]) ans[k++] = a[i++];
-            else ans[k++] = b[j++];
+            else {
+                // just 1 line logic change in code of merge sort
+                count += a.length - i;  // since a[i] > b[j], all elements a[i...end] will also be > b[j]
+                ans[k++] = b[j++];
+            }
         }
         while(i < l1) ans[k++] = a[i++];
         while(j < l2) ans[k++] = b[j++];
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,1,6,7,2,9,3,8,0,5};
-        mergeSort(arr);
-        for(int x : arr) System.out.print(x + " ");
+        int[] arr = {2, 4, 1, 3, 5};
+        System.out.println(inversionCount(arr));
     }
 }
